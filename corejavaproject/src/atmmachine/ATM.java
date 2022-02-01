6321d72357a331ed171d0006de52d26311da2c56
SUPRIYANAGARAJ <98803360+SUPRIYANAGARAJ@users.noreply.github.com>
2022-02-01 13:19:17 +0530

    Add files via upload

---
+++ corejavaproject/src/atmmachine/ATM.java
@@ -0,0 +1,16 @@
+package atmmachine;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+
+	public static void main(String[] args) throws IOException {
+		
+		OptionMenu optionmenu = new OptionMenu();
+		
+		optionmenu.getLogin();
+		
+
+	}
+
+}
